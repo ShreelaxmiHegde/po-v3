b8b5160c9835751fec41d5b7a286625557c1fb25
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-05-25 14:41:59 +0530

    add constants for social urls

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const SOCIALS = {
+  github: "https://github.com/ShreelaxmiHegde",
+  linkedin: "https://www.linkedin.com/in/shreelaxmi-hegde-832168358",
+  email: "mailto:shreelaxmihegde9@gmail.com",
+  url: "https://shreelaxmihegde.vercel.app"
+}
